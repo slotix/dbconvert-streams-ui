22b4d5c86ba2ce3a6fc3fd494498e4087a0e1692
slotix <dmsoft@gmail.com>
2023-08-15 00:10:56 +0200

    add save, update dialogs to steps

--- src/views/AddStreamView.vue
+++ src/views/AddStreamView.vue
@@ -12,11 +12,17 @@
       <div class="mb-20" v-if="currentStep !== null">
         <div v-show="currentStep.name === 'source'">
           <Connections :isSelectable="true" />
+          <Add v-if="dlgTp === 'Save'" />
+          <Edit v-if="dlgTp === 'Update'" />
         </div>
         <div v-show="currentStep.name === 'target'">
           <Connections :isSelectable="true" />
+          <Add v-if="dlgTp === 'Save'" />
+          <Edit v-if="dlgTp === 'Update'" />
         </div>
-        <div v-show="currentStep.name === 'settings'">Settings</div>
+        <div v-show="currentStep.name === 'settings'">
+          <Settings />
+        </div>
         <div v-show="currentStep.name === 'run'">run</div>
       </div>
     </div>
@@ -25,12 +31,19 @@
 <script setup>
 import { computed } from 'vue'
 import Connections from '@/components/connection/Connections.vue'
+import Add from '@/components/connection/Add.vue'
+import Edit from '@/components/connection/Edit.vue'
+import { useModalStore } from '@/stores/modalStore.js'
 import Steps from '@/components/stream/Steps.vue'
+import Settings from '@/components/settings/Settings.vue'
 import { useStreamsStore } from '@/stores/streams.js'
 import { mapState, mapWritableState } from 'pinia'
 const store = useStreamsStore()
 const allSteps = store.steps
 const currentStep = computed(() => {
   return store.currentStep
+})
+const dlgTp = computed(() => {
+  return useModalStore().dlgType
 })
 </script>
