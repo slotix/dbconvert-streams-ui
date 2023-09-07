eb2482d9735e9028b360947699fbfcb3d195c428
slotix <dmsoft@gmail.com>
2023-09-07 23:32:55 +0200

    change label

--- src/components/settings/ModeButtons.vue
+++ src/components/settings/ModeButtons.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="mt-6 flow-root">
       <RadioGroup v-model="mode" class="mt-2">
-        <RadioGroupLabel class="sr-only">Choose a memory option</RadioGroupLabel>
+        <RadioGroupLabel class="sr-only">Choose a data transfer mode</RadioGroupLabel>
         <div class="grid grid-cols-2 gap-3 sm:grid-cols-2">
           <RadioGroupOption as="template" v-for="option in modes" :key="option.id" :value="option"
             v-slot="{ active, checked }">
