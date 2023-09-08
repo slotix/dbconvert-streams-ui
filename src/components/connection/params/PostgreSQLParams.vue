602692924f251af64b7b4902319ba408d8c1959a
slotix <dmsoft@gmail.com>
2023-09-09 00:03:43 +0200

    change field order

--- src/components/connection/params/PostgreSQLParams.vue
+++ src/components/connection/params/PostgreSQLParams.vue
@@ -73,11 +73,11 @@
       </div>
       <hr />
       <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-        <label class="max-w-sm mx-auto md:w-1/3"> Database </label>
+        <label class="max-w-sm mx-auto md:w-1/3"> Schema </label>
         <div class="max-w-sm mx-auto md:w-2/3">
           <div class="relative">
             <input
-              v-model.lazy="connection.database"
+              v-model.lazy="connection.schema"
               type="text"
               class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"
               placeholder=""
@@ -86,11 +86,11 @@
         </div>
       </div>
       <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-        <label class="max-w-sm mx-auto md:w-1/3"> Schema </label>
+        <label class="max-w-sm mx-auto md:w-1/3"> Database </label>
         <div class="max-w-sm mx-auto md:w-2/3">
           <div class="relative">
             <input
-              v-model.lazy="connection.schema"
+              v-model.lazy="connection.database"
               type="text"
               class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent"
               placeholder=""
