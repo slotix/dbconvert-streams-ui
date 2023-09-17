e42ac64e535c50e43e61a72b68f38bb005894ff7
slotix <dmsoft@gmail.com>
2023-09-17 20:26:58 +0200

    fix a bug with layout

--- src/components/connection/DBTypesCombo.vue
+++ src/components/connection/DBTypesCombo.vue
@@ -1,6 +1,6 @@
 <template>
   <Combobox as="div" v-model="selectedDBType">
-    <div class="">
+    <div class="relative mt-2">
       <ComboboxInput
         class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
         @change="query = $event.target.value"
