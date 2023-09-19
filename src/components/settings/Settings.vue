ab401e986c2a20036a5f310321aafc29a18ce541
slotix <dmsoft@gmail.com>
2023-09-20 00:32:40 +0200

    change layout

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="antialiased bg-gray-200">
     <div class="grid grid-cols-2 gap-x-3 gap-y-6 mx-auto py-6 px-8">
+      <!-- First column -->
+      <div class="col-span-1">
+        <div class="flex flex-col">
+          <!-- TableList component with title -->
+          <TableList />
+        </div>
+      </div>
       <!-- Second column -->
       <div class="col-span-1">
         <div class="flex flex-col">
@@ -9,13 +16,6 @@
           <ModeButtons class="pb-8" />
           <!-- Limits component -->
           <Limits class="pb-8" />
-        </div>
-      </div>
-      <!-- First column -->
-      <div class="col-span-1">
-        <div class="flex flex-col">
-          <!-- TableList component with title -->
-          <TableList />
         </div>
       </div>
     </div>
