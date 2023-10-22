ea31a575bf204c3f9e1acc805a52279cbf54d9ba
slotix <dmsoft@gmail.com>
2023-10-23 01:31:09 +0200

    reformat file

--- src/components/settings/ModeButtons.vue
+++ src/components/settings/ModeButtons.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="px-4 sm:px-6 lg:px-8">
-
     <div class="flex items-center">
       <div class="flex-auto border-b border-gray-400 pb-5">
         <h3 class="text-base font-semibold leading-6 text-gray-900">Data transfer mode</h3>
@@ -10,15 +9,22 @@
       <RadioGroup v-model="mode" class="mt-2">
         <RadioGroupLabel class="sr-only">Choose a data transfer mode</RadioGroupLabel>
         <div class="grid grid-cols-2 gap-3 sm:grid-cols-2">
-          <RadioGroupOption as="template" v-for="option in modes" :key="option.id" :value="option"
-            v-slot="{ active, checked }">
-            <div :class="[
-              active ? 'ring-2 ring-gray-600 ring-offset-2' : '',
-              checked
-                ? 'bg-gray-600 text-white hover:bg-gray-500'
-                : 'ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50',
-              'flex items-center justify-center rounded-md py-3 px-3 text-sm font-normal   sm:flex-1'
-            ]">
+          <RadioGroupOption
+            as="template"
+            v-for="option in modes"
+            :key="option.id"
+            :value="option"
+            v-slot="{ active, checked }"
+          >
+            <div
+              :class="[
+                active ? 'ring-2 ring-gray-600 ring-offset-2' : '',
+                checked
+                  ? 'bg-gray-600 text-white hover:bg-gray-500'
+                  : 'ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50',
+                'flex items-center justify-center rounded-md py-3 px-3 text-sm font-normal   sm:flex-1'
+              ]"
+            >
               <RadioGroupLabel as="span">{{ option.title }}</RadioGroupLabel>
             </div>
           </RadioGroupOption>
@@ -34,7 +40,7 @@ import { useStreamsStore } from '@/stores/streams.js'
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 const modes = useStreamsStore().modes
 const currentStream = useStreamsStore().currentStream
-const mode = ref(modes.find(option => option.id === currentStream.mode) || modes[0]);
+const mode = ref(modes.find((option) => option.id === currentStream.mode) || modes[-1])
 watch(mode, (newVal) => {
   currentStream.mode = newVal.id
 })
