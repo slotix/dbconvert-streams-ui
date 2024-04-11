485d349d95f2c7860506e0b52138270f7517e270
slotix <dmsoft@gmail.com>
2024-04-11 20:29:13 +0200

    add empty state for monitoring view

--- src/views/MonitorStreamView.vue
+++ src/views/MonitorStreamView.vue
@@ -7,7 +7,7 @@
     </div>
   </header>
   <main>
-    <div class="antialiased bg-gray-200">
+    <div v-if="monitoringStore.streamID != ''" class="antialiased bg-gray-200">
       <div class="flex flex-wrap max-w-7xl mx-auto px-4 overflow-hidden">
         <div class="w-full px-4 overflow-hidden mb-10">
           <ProgressContainer />
@@ -16,18 +16,28 @@
         </div>
       </div>
     </div>
+    <div v-else class ="antialiased bg-gray-200">
+      <div class="text-center pt-16 pb-16">
+        <ChartBarSquareIcon class=" mx-auto h-16 w-16 text-gray-400" />
+        <h3 class="mt-2 text-xl font-semibold text-gray-900">No Active Streams</h3>
+        <p class="mt-1 text-lg text-gray-700">Currently, there are no ongoing data transfer processes.</p>
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script setup>
 
 import { onMounted } from 'vue';
+import { ChartBarSquareIcon } from '@heroicons/vue/20/solid'
 import LogContainer from '@/components/monitoring/LogContainer.vue';
 import StatContainer from '@/components/monitoring/StatContainer.vue';
 import ProgressContainer from '@/components/monitoring/ProgressContainer.vue';
 
 import { useMonitoringStore } from '@/stores/monitor.js'
-const monitoringStore = useMonitoringStore()
+const monitoringStore = useMonitoringStore();
+
 onMounted(() => {
   monitoringStore.consumeLogsFromNATS();
 });
