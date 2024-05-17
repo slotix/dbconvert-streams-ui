c19eb4296bcc880461682a796490777746d2a0ef
slotix <dmsoft@gmail.com>
2024-05-17 14:12:47 +0200

    fix layout

--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -2,11 +2,11 @@
   <div class="bg-gray-200">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
       <div class="lg:flex lg:flex-row">
-        <div class="lg:w-1/2">
+        <div class="lg:w-1/2 pb-8">
           <TableList />
         </div>
         <div class="lg:w-1/2 lg:pl-8">
-          <StreamSettings class="pb-8 pt-8" />
+          <StreamSettings class="pb-8" />
           <TableSettings class="pb-8" />
         </div>
       </div>
