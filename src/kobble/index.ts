97b78831e654355d31119bd378663e57a62b17ff
slotix <dmsoft@gmail.com>
2024-06-19 22:24:46 +0200

    remove obsolete code

--- src/kobble/index.ts
+++
@@ -1,9 +0,0 @@
-// src/kobble/index.ts
-
-import { createKobbleClient } from '@kobbleio/javascript'
-
-export const kobbleClient = createKobbleClient({
-    domain: import.meta.env.VITE_KOBBLE_DOMAIN,
-    clientId: import.meta.env.VITE_KOBBLE_CLIENT_ID,
-    redirectUri: import.meta.env.VITE_KOBBLE_REDIRECT_URI
-})
