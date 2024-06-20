530f3fe1865733cb41eccae2dbd9c5fecd721b65
slotix <dmsoft@gmail.com>
2024-06-20 10:51:29 +0200

    change title when there are no streams

--- src/views/StreamsView.vue
+++ src/views/StreamsView.vue
@@ -2,7 +2,10 @@
   <header>
     <div class="bg-white flex flex-wrap justify-between space-y-4 sm:space-y-0 max-w-7xl mx-auto py-6 px-8">
       <h1 class="flex-auto text-3xl font-bold text-gray-900 dark:text-white;">
-        {{ streamsCount() }} Stream configurations.
+        <span v-if="streamsCount() > 0">
+          {{ streamsCount() }} Stream configurations.
+        </span>
+        <span v-else>No Stream configurations.</span>
       </h1>
     </div>
   </header>
