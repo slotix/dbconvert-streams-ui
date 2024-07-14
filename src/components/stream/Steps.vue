aeff378795c4f8e298ee0ab48bebdc49793be821
slotix <dmsoft@gmail.com>
2024-07-14 17:22:38 +0200

    move buttons prev, next save to the bottom

--- src/components/stream/Steps.vue
+++ src/components/stream/Steps.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-auto">
+  <div class="relative">
     <div v-for="step in allSteps" :key="step.id">
       <div v-show="currentStepIndex === step.id">
         <div class="uppercase tracking-wide text-sm font-bold text-gray-500 mb-1 leading-tight">
@@ -17,32 +17,33 @@
           </div>
         </div>
         <div class="flex flex-row">
-          <img class="mr-3 h-10 w-10" :src="step.img" :alt="step.title" />
-          <div class="text-xl font-bold text-gray-700 leading-tight">
+          <img class="mr-3 h-8 w-8" :src="step.img" :alt="step.title" />
+          <div class="mt-1 text-xl  text-gray-700 leading-tight">
             {{ step.title }}
           </div>
         </div>
       </div>
     </div>
-    <div class="my-6 sm:flex sm:flex-row">
+    <div class="fixed bottom-20 right-4 flex space-x-2 z-50">
       <button type="button" @click="prev" :disabled="currentStepIndex === 1" aria-label="Go to previous step"
-        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto disabled:opacity-50 disabled:cursor-not-allowed">
+        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:w-auto disabled:opacity-50 disabled:cursor-not-allowed">
         <ChevronLeftIcon class="h-6 w-6" aria-hidden="true" />
         Back
       </button>
       <button type="button" @click="next" :disabled="currentStepIndex === stepsCount" aria-label="Go to next step"
-        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto disabled:opacity-50 disabled:cursor-not-allowed">
+        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:w-auto disabled:opacity-50 disabled:cursor-not-allowed">
         Next
         <ChevronRightIcon class="h-6 w-6" aria-hidden="true" />
       </button>
       <button type="button" @click="saveStream"
-        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:ml-3 sm:w-auto disabled:opacity-50 disabled:cursor-not-allowed"
+        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:w-auto disabled:opacity-50 disabled:cursor-not-allowed"
         :disabled="isSaveDisabled">
         Save
       </button>
     </div>
   </div>
 </template>
+
 <script setup>
 import { useStreamsStore } from '@/stores/streams'
 import { useCommonStore } from '@/stores/common'
