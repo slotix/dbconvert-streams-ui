bdcd451a6defea30b9f06dbea2d98a39d67120c2
slotix <dmsoft@gmail.com>
2024-10-17 21:34:40 +0200

    make stream name smaller

--- src/components/stream/CardItem.vue
+++ src/components/stream/CardItem.vue
@@ -17,7 +17,7 @@
         </span>
       </div>
       <div class="flex-grow p-4 space-y-3 text-sm">
-        <div class="text-gray-800 font-bold text-lg mb-2">
+        <div class="text-gray-800 font-bold text-sm mb-2">
           {{ stream.name }}
         </div>
         <div class="text-xs text-gray-500 flex items-center">
