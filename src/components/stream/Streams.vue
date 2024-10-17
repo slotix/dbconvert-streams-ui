928e91a5d32a8a9862b60f151b92a9bfefad4103
slotix <dmsoft@gmail.com>
2024-10-17 22:17:47 +0200

    remove new card. Add new "Add stream" button

--- src/components/stream/Streams.vue
+++ src/components/stream/Streams.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="antialiased bg-gray-200">
-    <div class="flex flex-wrap gap-x-3 space-y-0 max-w-7xl mx-auto py-6 px-8;">
+    <div class="flex flex-wrap gap-x-3 space-y-0 max-w-7xl mx-auto py-6 px-8 justify-between">
       <ToggleView class="py-2 px-8" />
+      <div class="flex-grow"></div>
+      <router-link :to="{ name: 'ManageStream', params: { mode: 'add' } }">
+        <button type="button"
+          class="py-2 px-4 mt-2 flex items-center justify-center rounded-md bg-gray-600 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+          @click="addStream">
+          <PlusIcon class="mr-2 h-5 w-5 text-white" aria-hidden="true" />
+          Add stream
+        </button>
+      </router-link>
     </div>
     <div v-if="streams.length > 0" class="flex flex-wrap max-w-7xl mx-auto px-4 overflow-hidden"
       v-show="viewType === 'cards'">
-      <div class="w-full px-4 overflow-hidden md:w-1/2 lg:w-1/3">
+      <!-- <div class="w-full px-4 overflow-hidden md:w-1/2 lg:w-1/3">
         <NewCard />
-      </div>
+      </div> -->
       <div class="w-full px-4 overflow-hidden md:w-1/2 lg:w-1/3" v-for="stream in streams" :key="stream.id">
         <CardItem :stream="stream" :source="connectionByID(stream.source)" :target="connectionByID(stream.target)" />
       </div>
@@ -33,6 +42,8 @@ import { useStreamsStore } from '@/stores/streamConfig';
 import { useConnectionsStore } from '@/stores/connections';
 import { useCommonStore } from '@/stores/common';
 import { mapActions } from 'pinia';
+import { PlusIcon } from '@heroicons/vue/24/solid';
+import { useRouter } from 'vue-router';
 
 export default {
   name: 'Streams',
@@ -41,6 +52,7 @@ export default {
     ToggleView,
     CardItem,
     NewCard,
+    PlusIcon,
   },
 
   setup() {
@@ -58,6 +70,14 @@ export default {
 
     const viewType = computed(() => commonStore.currentViewType);
     const streams = computed(() => streamsStore.newestFirst);
+    const router = useRouter();
+
+    const addStream = () => {
+      streamsStore.resetCurrentStream();
+      connectionsStore.resetCurrentConnection();
+      // router.push({ name: 'ManageStream', params: { mode: 'add' } });
+    };
+
     onMounted(async () => {
       try {
         await connectionsStore.refreshConnections();
@@ -75,7 +95,8 @@ export default {
         return connectionsStore.connectionByID(id);
       },
       viewType,
-      streams
+      streams,
+      addStream,
     };
   },
 };
