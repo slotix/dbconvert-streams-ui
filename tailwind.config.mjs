5f46c2a60fdbcc570a45b027c48c9ac63d7027ad
slotix <dmsoft@gmail.com>
2024-10-27 02:09:06 +0200

    rename tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 import colors from 'tailwindcss/colors'
 import forms from '@tailwindcss/forms'
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
