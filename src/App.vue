b52132f559ae7d1e91ddce63b677e7b74349eab2
slotix <dmsoft@gmail.com>
2024-11-07 23:12:23 +0100

    Close sidebar automatically after selecting a menu item

--- src/App.vue
+++ src/App.vue
@@ -64,6 +64,7 @@
                             : 'text-gray-400 hover:text-white hover:bg-gray-800',
                           'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                         ]"
+                        @click="sidebarOpen = false"
                       >
                         <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
                         {{ item.name }}
