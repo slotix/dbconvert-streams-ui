a0fc23ed010d9912bb4caa1deadce0e2337a5c7b
slotix <dmsoft@gmail.com>
2025-12-10 22:33:40 +0100

    feat: simplify key generation for TableSettings component

--- src/components/settings/TableList.vue
+++ src/components/settings/TableList.vue
@@ -336,7 +336,7 @@
                   class="col-span-1 lg:col-span-2 px-4 py-3 bg-gray-50 dark:bg-gray-800/30 border-b border-gray-200 dark:border-gray-700"
                 >
                   <TableSettings
-                    :key="`${table.name}-${JSON.stringify(table.filter)}`"
+                    :key="table.name"
                     :table="table"
                   />
                 </div>
